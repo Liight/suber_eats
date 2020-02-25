cfcd1aeda80534857c9bfcc3075d1708bc59ac90
Liight <Liight@users.noreply.github.com>
2020-02-26 07:05:00 +1300

    - handle unregistered named routes - handle unknown routes

--- lib/main.dart
+++ lib/main.dart
@@ -40,6 +40,14 @@ class MyApp extends StatelessWidget {
         CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
         MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
       },
+      // Load this route for any named route that is not registered above
+      onGenerateRoute: (settings){
+        print(settings.arguments);
+        return MaterialPageRoute(builder: (ctx) => CategoriesScreen(),);
+      },
+      onUnknownRoute: (settings){
+        return MaterialPageRoute(builder: (ctx) => CategoriesScreen(),);
+      },
     );
   }
 }
